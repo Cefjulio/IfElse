e4a4b8ace1f4b061c586cf336a38e87192e257f5
Cefnar <Cefnar@clientew10>
2018-03-10 22:48:47 -0600

    Commit test

---
+++ src/ejemploifelse/EjemploIfElse.java
@@ -0,0 +1,42 @@
+
+import java.util.Locale;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ 
+package ejemploifelse;
+
+/**
+ *
+ * @author Cefnar
+ */
+public class EjemploIfElse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 10;
+        
+        if (x==10){
+            System.out.print("X es igual a  10\n");
+            
+        }else if (x==20){
+            System.out.print("X es igual a 20\n");
+            
+        }else if (x==30){
+            System.out.print("X es igual a 30\n");
+        
+        } else {
+            System.out.println("X no es igual ni a 10, ni a 20, ni a 30 \n");
+        
+        }
+        
+        
+        
+    }
+    
+}
